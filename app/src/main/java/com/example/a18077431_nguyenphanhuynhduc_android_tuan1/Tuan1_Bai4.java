9351476b0d21c54e0569861c7b0429de5483c8d3
duc <3beonhi3@gmail.com>
2021-05-16 14:55:48 +0700

    done

---
+++ app/src/main/java/com/example/a18077431_nguyenphanhuynhduc_android_tuan1/Tuan1_Bai4.java
@@ -0,0 +1,14 @@
+package com.example.a18077431_nguyenphanhuynhduc_android_tuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tuan1_Bai4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan1__bai4);
+    }
+}
